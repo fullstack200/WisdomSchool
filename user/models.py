1cd1d29b8fe7406d48bebefae742eddce7015a5b
Ahmed <fullstackdeveloper404@gmail.com>
2024-04-06 16:54:40 +0530

    Project update completed.
    Project update completed.

--- user/models.py
+++ user/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 
-class Testimonials(models.Model):
-    name = models.CharField(max_length=50)
-    emailOrPhone = models.CharField(max_length=50,blank=True)
-    comments = models.TextField(max_length=500)
+class Enquiries(models.Model):
+    name = models.CharField(max_length=50,null = True)
+    studentPhone = models.CharField(max_length=50,null = True)
+    studentEmail = models.CharField(max_length=50,null = True)
+    message = models.TextField(max_length=500,null = True)
 
     def __str__(self):
         return self.name[:50]
-
