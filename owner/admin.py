3b41e8226cd6afc5658ff66a6b5bb9a7d3153f73
Ahmed <fullstackdeveloper404@gmail.com>
2024-11-23 14:37:05 +0530

    Added DistinctionHolder model associated with YearOfExam model in order to add multiple toppers.

--- owner/admin.py
+++ owner/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Pictures,Toppers,Announcements
+from .models import Pictures,YearOfExam,Announcements,DistinctionHolder
 # Register your models here.
 
 admin.site.register(Pictures)
-admin.site.register(Toppers)
+admin.site.register(YearOfExam)
 admin.site.register(Announcements)
+admin.site.register(DistinctionHolder)
